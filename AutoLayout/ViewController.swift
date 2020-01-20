524ef1c29e0104ed35fa4261959fb492d933a141
nomizo-iox <nomizo.iox@gmail.com>
2020-01-19 20:51:04 -0600

    Adding all the initial files to master.

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Samuel F. Ademola on 1/19/20.
+//  Copyright © 2020 Nomizo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
